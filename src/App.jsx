8f12c138670e3e16bb2def36230d52a8e768d6fd
Stefanni Brasil <stefannibrasil@gmail.com>
2022-11-16 08:29:29 -0800

    Pass items as props

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,25 @@
 import * as React from 'react';
 
-const javascript = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-const ruby = [
-  {
-    title: 'Bottles of OOP',
-    url: 'https://sandimetz.com/99bottles',
-    author: 'Sandi Metz',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'PODR',
-    url: 'https://www.informit.com/store/practical-object-oriented-design-an-agile-primer-using-9780134456478?ranMID=24808',
-    author: 'Sandi Metz',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-function Item({item}) {
-  return (
-    <div>
+const Item = (props) => (
+  <li>
+    <span>
       <h2>
-        <a href={item.url}>{item.title}</a>
+        <a href={props.item.url}>{props.item.title}</a>
       </h2>
-      <h3> Author: {item.author}</h3>
-      <p> Comments: {item.num_comments}</p>
-      <p> Points: {item.points}</p>
-    </div>
-  );
-}
+      <h3> Author: {props.item.author}</h3>
+      <p> Comments: {props.item.num_comments}</p>
+      <p> Points: {props.item.points}</p>
+    </span>
+  </li>
+);
 
-function List ({list}) {
-  return (
-    <ul>
-      {list.map(function (item) {
-      return (
-        <li key={item.objectID}>
-          <Item item={item} />
-        </li>
-      )
-    })}
-    </ul>
-  );
-}
+const List = (props) => (
+  <ul>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
+  ))}
+  </ul>
+);
 
 const Search = () => {
   const handleChange = (event) => {
@@ -86,7 +42,45 @@ const Search = () => {
   );
 }
 
-function App() {
+const App = () => {
+  const javascript = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+  
+  const ruby = [
+    {
+      title: 'Bottles of OOP',
+      url: 'https://sandimetz.com/99bottles',
+      author: 'Sandi Metz',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'PODR',
+      url: 'https://www.informit.com/store/practical-object-oriented-design-an-agile-primer-using-9780134456478?ranMID=24808',
+      author: 'Sandi Metz',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
@@ -95,11 +89,11 @@ function App() {
 
       <hr />
 
-      <List list = {ruby} />
+      <List list={ruby} />
 
-      <List list = {javascript} />
+     <List list={javascript} />
     </div>
   );
-}
+};
  
 export default App;
