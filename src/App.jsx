7678189c482abbd66b43fd7051a3ae52387d6ab1
Stefanni Brasil <stefannibrasil@gmail.com>
2022-12-06 08:41:08 -0800

    Component composition

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,9 @@ const List = ({ list }) => (
   </ul>
 );
 
-const InputWithLabel = ({ id, label, value, type = 'text', onInputChange }) => (
+const InputWithLabel = ({ id, value, type = 'text', onInputChange, children }) => (
   <>
-    <label htmlFor={id}>{label}:</label>
+    <label htmlFor={id}>{children}</label>
     &nbsp;
     <input id={id} type={type} value={value} onChange={onInputChange}/>
   </>
@@ -97,7 +97,12 @@ const App = () => {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <InputWithLabel id="search" label="Search" value={searchTerm} onInputChange={handleSearch} />
+      <InputWithLabel
+        id="search"
+        value={searchTerm}
+        onInputChange={handleSearch}>
+        <strong>Search:</strong>
+      </InputWithLabel>
 
       <hr />
 
