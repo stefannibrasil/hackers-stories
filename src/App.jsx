9ef82aed236c665b9c5e3cede8685cc7509e308b
Stefanni Brasil <stefannibrasil@gmail.com>
2022-12-12 16:14:47 -0800

    Imperative React

--- src/App.jsx
+++ src/App.jsx
@@ -19,13 +19,23 @@ const List = ({ list }) => (
   </ul>
 );
 
-const InputWithLabel = ({ id, value, type = 'text', onInputChange, children }) => (
-  <>
-    <label htmlFor={id}>{children}</label>
-    &nbsp;
-    <input id={id} type={type} value={value} onChange={onInputChange}/>
-  </>
-)
+const InputWithLabel = ({ id, value, type = 'text', onInputChange, isFocused, children }) => {
+  const inputRef = React.useRef();
+
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus()
+      }
+  }, [isFocused]);
+
+  return(
+    <>
+      <label htmlFor={id}>{children}</label>
+      &nbsp;
+      <input id={id} type={type} value={value} ref={inputRef} onChange={onInputChange}/>
+    </>
+  )
+}
 
 const useStorageState = (key, initialState) => {
   const [value, setValue] = React.useState(
