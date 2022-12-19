f409503495e434934df0391bf3f72222ad86c38e
Stefanni Brasil <stefannibrasil@gmail.com>
2022-12-19 08:38:27 -0800

    Async/Await

--- src/App.jsx
+++ src/App.jsx
@@ -113,19 +113,19 @@ const App = () => {
     {data: [], isLoading: false, isError: false}
   );
 
-  const handleFetchStories = React.useCallback(() => {
+  const handleFetchStories = React.useCallback(async () => {
     dispatchStories({type: 'STORIES_FETCH_INIT'});
 
-    axios
-    .get(url)
-    .then((result) => {
+    try {
+      const result = await axios.get(url);
+
       dispatchStories({
         type: 'STORIES_FETCH_SUCCESS',
         payload: result.data.hits,
       });
-    })
-    .catch(() =>
-      dispatchStories({type: 'STORIES_FETCH_ERROR'}));
+    } catch {
+      dispatchStories({ type: 'STORIES_FETCH_FAILURE'})
+    }
   }, [url]);
 
   React.useEffect(() => {
